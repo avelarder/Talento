f031c5360c1d161aaf5e4209881b8b21b67b4884
Fabian Gargaglione <fabian.gargaglione.tcs@outlook.com>
2017-03-23 16:38:21 -0300

    311 - Dashboard Index Controller by Role

--- Talento/Talento/Controllers/DashboardController.cs
+++ Talento/Talento/Controllers/DashboardController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Talento.Controllers
 {
-    [Authorize(Roles = "Basic")]
+    //[Authorize(Roles = "Basic")]
+    [Authorize]
     public class DashboardController : Controller
     {
         // GET: Dashboard
         public ActionResult Index()
         {
+            string Dashboard = "_PartialContent.cshtml";
+            string Role = "basic";
+
+            if (Roles.IsUserInRole("Admin"))
+            {
+                Dashboard = "_PartialContentAdmin.cshtml";
+                Role = "admin";
+            }
+            ViewData["RoleClass"] = Role + "-role";
+            ViewData["Dashboard"] = Dashboard;
+            ViewData["Role"] = Role;
             return View();
         }
     }
