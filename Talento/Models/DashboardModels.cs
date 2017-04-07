4599c96a52ed3ed201687be9c4be2a59bb54d52d
Carlos Hackenbruch <chackenbruch@outlook.com>
2017-04-07 14:15:19 -0300

    Setting up solution for demo.

--- Talento/Models/DashboardModels.cs
+++ Talento/Models/DashboardModels.cs
@@ -27,6 +27,7 @@ namespace Talento.Models
 
         [Required(ErrorMessage = "Engagement Manager is required")]
         [DisplayName("Engagement Manager")]
+        [Range(1, 20)]
         public string EngagementManager { get; set; }
 
         [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid RGS")]
@@ -58,6 +59,7 @@ namespace Talento.Models
 
         [Required(ErrorMessage = "Engagement Manager is required")]
         [DisplayName("Engagement Manager")]
+        [Range(1, 20)]
         public string EngagementManager { get; set; }
 
         public string PortfolioManager_Id { get; set; }
@@ -116,6 +118,7 @@ namespace Talento.Models
         public string Area { get; set; }
 
         [Required(ErrorMessage = "Engagement Manager is required")]
+        [Range(1, 20)]
         public string EngagementManager { get; set; }
 
         [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid RGS")]
