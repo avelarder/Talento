0f16cbfee68a826e09482e9ce1c63318915702e5
Pablo <pablorcarmona@hotmail.com>
2017-04-11 12:23:22 -0300

    CandidateController created

---
+++ Talento/Controllers/CandidateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Talento.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public CandidateController()
+        {
+
+        }
+        
+        public void SendMail()
+        {
+            
+        }
+    }
+}
