b0136a87015958a64ce93a2e061c676b9c40e511
Martin Mato <martinmato90@outlook.com>
2017-04-20 16:48:54 -0300

    Edit candidate controller unit test added

--- Talento.Tests/Controllers/CandidateControllerTest.cs
+++ Talento.Tests/Controllers/CandidateControllerTest.cs
@@ -90,5 +90,80 @@ namespace Talento.Tests.Controllers
             Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
         }
+
+        [TestMethod]
+        public void EditTest()
+        {
+            //Mocking interfaces and entities needed
+            var mocks = new MockRepository(MockBehavior.Default);
+            Mock<ICandidate> mockCandidateHelper = mocks.Create<ICandidate>();
+            Mock<IPrincipal> mockPrincipal = mocks.Create<IPrincipal>();
+            Mock<IPosition> mockPositionHelper = mocks.Create<IPosition>();
+            Mock<ICustomUser> mockUserHelper = mocks.Create<ICustomUser>();
+            Mock<IFileManagerHelper> mockFileManagerHelper = mocks.Create<IFileManagerHelper>();
+            var mockContext = new Mock<ControllerContext>();
+            Mock<ApplicationUser> mockUser = mocks.Create<ApplicationUser>();
+
+            //Setting up the context
+            mockPrincipal.Setup(p => p.IsInRole("PM")).Returns(true);
+            mockPrincipal.SetupGet(p => p.Identity.Name).Returns("pablo@example.com");
+            mockContext.Setup(p => p.HttpContext.User.Identity.Name).Returns(mockPrincipal.Object.Identity.Name);
+            mockContext.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+
+            //Creating test data
+            var userTest = new ApplicationUser()
+            {
+                Id = "pablo@example.com",
+                Email = "pablo@example.com"
+            };
+            var positionTest = new Position()
+            {
+                Id = 1,
+                Title = "aTitle",
+            };
+            Candidate candidate = new Candidate {
+                Email = "Candidate00@Example.com"
+            };
+            byte[] blob = new byte[1];
+            List<FileBlob> files = new List<FileBlob>()
+            {
+                new FileBlob { Id = 1, Candidate_Id = 1, FileName = "aFile", Candidate = candidate, Blob = blob },
+                new FileBlob { Id = 2, Candidate_Id = 1, FileName = "aFile1", Candidate = candidate, Blob = blob },
+                new FileBlob { Id = 3, Candidate_Id = 1, FileName = "aFile2", Candidate = candidate, Blob = blob }
+            };
+            EditCandidateViewModel candidateViewModel = new EditCandidateViewModel
+            {
+                Id = 1,
+                Position_Id = 1,
+                Competencies = "someCompetencies",
+                CratedOn = DateTime.Now,
+                CreatedBy = mockUser.Object,
+                Description = "aDescription",
+                Email = "aMail@tcs.com",
+                Name = "pepito",
+                IsTcsEmployee = "on",
+                Status = CandidateStatus.New,
+                CreatedBy_Id = "1"
+            };
+
+            //Creating the controller and sending de test data to it
+            mockUserHelper.Setup(p => p.GetUserByEmail("pablo@example.com")).Returns(userTest);
+            mockPositionHelper.Setup(p => p.Get(1)).Returns(positionTest);
+            mockCandidateHelper.Setup(p => p.Get(1)).Returns(candidate);
+            CandidateController controller = new CandidateController(mockCandidateHelper.Object, mockUserHelper.Object,
+                                                mockFileManagerHelper.Object,
+                                                mockPositionHelper.Object)
+            {
+                ControllerContext = mockContext.Object
+            };
+            mockContext.SetupGet(c => c.HttpContext.Session["files"]).Returns(files);
+
+            //Act
+            var result = controller.Edit(candidateViewModel);
+
+            //Asserts
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+        }
     }
 }
