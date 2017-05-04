6738dfd7afd6f19eccc9b0097e6f500bbe2df4c1
Martin Mato <martinmato90@outlook.com>
2017-05-04 14:08:09 -0300

    Created TechnicalInterview entity and added method to add it to a candidate.

---
+++ Talento.Entities/TechnicalInterview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talento.Entities
+{
+    public class TechnicalInterview
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TechnicalInterviewId { get; set; }
+
+        [Required]
+        public virtual PositionCandidates PositionCandidate { get; set; }
+
+        [Required]
+        public DateTime? Date { get; set; }
+
+        [Required]
+        public bool IsAccepted { get; set; }
+
+        [StringLength(500, ErrorMessage = "Comment must have 500 characters maximum")]
+        public string Comment { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Interviewer Id must have 10 characters maximum")]
+        public string InterviewerId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Interviewer Name must have 50 characters maximum")]
+        public string InterviewerName { get; set; }
+
+        [Required]
+        public FileBlob FeedbackFile { get; set; }
+    }
+}
