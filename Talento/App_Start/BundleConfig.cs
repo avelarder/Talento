febc9ef017e1164267df647b9b36f19c51d075af
Federico Testoni <eltesto@hotmail.com>
2017-05-11 00:48:26 -0300

    - Styles fixed for login

--- Talento/App_Start/BundleConfig.cs
+++ Talento/App_Start/BundleConfig.cs
@@ -15,9 +15,9 @@ namespace Talento
           "~/Content/PagedList.css",
           "~/Content/toastr.min.css",
           "~/Content/alertify.min.css",
-          "~/Content/select2-bootstrap.css",
-          "~/Content/normalize.css",
-          "~/Content/styles-covervid.css"
+          "~/Content/select2-bootstrap.css"
+          //"~/Content/normalize.css",
+          //"~/Content/styles-covervid.css"
           ));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
