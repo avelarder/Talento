76e915f73a2fca580fa79d598de77b5e32b30167
Martin Mato <martinmato90@outlook.com>
2017-05-16 17:57:22 -0300

    Added ChangeImageProfileController and unit test

---
+++ Talento/Models/ChangeImageProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Talento.Models
+{
+    public class ChangeImageProfileViewModel
+    {
+        public Image profileImage { get; set; }
+
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
